417a1d5f905fa2f1bacfcdc56d36fd4df041cebf
Sven Trieflinger <sven.trieflinger@de.bosch.com>
2023-01-16 17:21:40 +0100

    First steps towards model owner implementation
    Signed-off-by: Sven Trieflinger <sven.trieflinger@de.bosch.com>

---
+++ model_owner/model_owner.py
@@ -0,0 +1,21 @@
+import logging
+import grpc
+
+from generated.model_training_pb2 import TrainModelParameters
+from generated.model_training_pb2_grpc import ModelTrainingStub
+
+
+def train_model(stub):
+    params = TrainModelParameters()
+    final_model_id = stub.TrainModel(params)
+
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = ModelTrainingStub(channel)
+        train_model(stub)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
